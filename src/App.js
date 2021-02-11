6e2c0d24a27cf5f8b7cd85689b5980fa00fc5233
harshsit <harsh.raj@sitpune.edu.in>
2021-02-11 21:02:45 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  STATUS,
+  Loading,
+  Avatar,
+  Logo,
+  Logotype,
+  Container,
+  Header
+} from "gitstar-components";
+
+const CLIENT_ID = "ce68c3b9cb4f1035a0af";
+const REDIRECT_URI = "http://localhost:3000/";
+
+class App extends Component {
+  state = {
+    status: STATUS.INITIAL,
+    token: null
+  };
+
+  // componentDidMount() {
+  //   const code =
+  //     window.location.href.match(/?code=(.*)/) &&
+  //     window.location.href.match(/?code=(.*)/)[1];
+  //   console.log(code);
+  // }
+  render() {
+    return (
+      <Container>
+        <Header>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Logo />
+            <Logotype />
+          </div>
+          <a
+            href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+          >
+            Login
+          </a>
+        </Header>
+      </Container>
+    );
+  }
 }
 
 export default App;
